815546716a79b46d6ebf0280c93e733a5393f26b
Dev6Socializus <72788978+Stinger1369@users.noreply.github.com>
2023-11-20 13:22:24 +0100

    2023-11-20-13h22-Bilel
    add props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,10 +3,26 @@
 import PokemonCard  from './components/PokemonCard';
 import './App.css';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    //imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+];
 function App() {
     return (
         <div>
-            <PokemonCard />
+          {/* pour afficher une seule pokemon */}
+            {/* <PokemonCard pokemon={pokemonList[0]} /> */}
+            <div>
+            {pokemonList.map((pokemon, index) => (
+                <PokemonCard key={index} pokemon={pokemon} />
+            ))}
+        </div>
         </div>
     );
 }
--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -4,47 +4,36 @@ import '../assets/Pokemon.css';
 
 
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-  ];
-//3. Contenu du composant : dans le fichier PokemonCard.jsx, crée un composant React appelé PokemonCard. Ce composant doit contenir une balise 
-// function PokemonCard() {
-//     const pokemon = pokemonList[1]; // Pour afficher le premier Pokémon
 
-//     return (
-//         <figure className='Pokedox'>
-//             {pokemon.imgSrc ? (
-//                 <img src={pokemon.imgSrc} alt={pokemon.name} />
-//             ) : (
-//                 <p></p>
-//             )}
-//             <figcaption>{pokemon.name}</figcaption>
-//         </figure>
-//     );
-// }
-function PokemonCard() {
+//3. Contenu du composant : dans le fichier PokemonCard.jsx, crée un composant React appelé PokemonCard. Ce composant doit contenir une balise 
+function PokemonCard({ pokemon }) {
     return (
-        <div>
-            {pokemonList.map((pokemon, index) => (
-                <figure key={index} className='Pokedox'>
-                    {pokemon.imgSrc ? (
-                        <img src={pokemon.imgSrc} alt={pokemon.name} />
-                    ) : (
-                        <p>???</p>
-                    )}
-                    <figcaption className='title'>{pokemon.name}</figcaption>
-                </figure>
-            ))}
-        </div>
+        <figure className='Pokedox'>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
     );
 }
+// function PokemonCard(props) {
+//     return (
+//         <div>
+//             {pokemonList.map((pokemon, index) => (
+//                 <figure key={index} className='Pokedox'>
+//                     {pokemon.imgSrc ? (
+//                         <img src={pokemon.imgSrc} alt={pokemon.name} />
+//                     ) : (
+//                         <p>???</p>
+//                     )}
+//                     <figcaption className='title'>{pokemon.name}</figcaption>
+//                 </figure>
+//             ))}
+//         </div>
+//     );
+// }
   
 
 export default PokemonCard;
