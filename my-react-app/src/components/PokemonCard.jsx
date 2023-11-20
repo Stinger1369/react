c7430b6b45eee96520e4f042983688054a990c15
Dev6Socializus <72788978+Stinger1369@users.noreply.github.com>
2023-11-20 11:45:43 +0100

    2023-11-20-11h45-Bilel
    add style

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,19 @@
+//inmportation style css from assets/Pokemon.css
+import '../assets/Pokemon.css';
+
+
+//3. Contenu du composant : dans le fichier PokemonCard.jsx, crée un composant React appelé PokemonCard. Ce composant doit contenir une balise 
+function PokemonCard() {
+    return (
+        <figure className='Pokedox'>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    );
+}
+
+export default PokemonCard;
+
+
+
+
